eb02fd5e2a03ccd011a025241f967be16d717ad6
somundane <98dana@gmail.com>
2025-12-14 18:18:47 -0500

    Decent Working Rewards

--- source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env_cfg.py
+++ source/rob6323_go2/rob6323_go2/tasks/direct/rob6323_go2/rob6323_go2_env_cfg.py
@@ -100,21 +100,21 @@ class Rob6323Go2EnvCfg(DirectRLEnvCfg):
     current_vel_visualizer_cfg.markers["arrow"].scale = (0.5, 0.5, 0.5)
 
     # reward scales
-    lin_vel_reward_scale = 1.0
-    yaw_rate_reward_scale = 0.5
+    lin_vel_reward_scale = 1.0  # originally 0.5
+    yaw_rate_reward_scale = 0.5  # originally 0.5
 
-    action_rate_reward_scale = -0.1     # originally -0.1 try -0.5
+    action_rate_reward_scale = -0.2    # originally -0.1
 
     # Additional reward scales
     orient_reward_scale = -5.0
-    lin_vel_z_reward_scale = -0.02  # originally -0.02 try -0.06
-    dof_vel_reward_scale = -0.0001
-    ang_vel_xy_reward_scale = -0.001
+    lin_vel_z_reward_scale = -0.02  # originally -0.02
+    dof_vel_reward_scale = -0.0001  # originally -0.0001
+    ang_vel_xy_reward_scale = -0.001  # originally -0.001
 
     raibert_heuristic_reward_scale = -10.0  # originally -10.0
     # feet_clearance_reward_scale = -30.0
     # tracking_contacts_shaped_force_reward_scale = 4.0
 
-    feet_clearance_reward_scale = -30.0  # originally -30.0 try -25
-    contact_force_reward_scale = 4  # originally 4 try 6
-    tracking_contacts_shaped_force_reward_scale = 4.0
+    feet_clearance_reward_scale = -40.0  # originally -30.0
+    contact_force_reward_scale = 8.0  # originally 4
+    # tracking_contacts_shaped_force_reward_scale = 4.0  # originally 4.0
